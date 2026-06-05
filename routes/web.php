919a923d050b028c879ca5c1977b10cd29fd22bf
Falu Zaraj SM <faluzaraj@gmail.com>
2026-06-05 15:45:23 -0500

    Adaptación de cambios del modal de "pagos" agregado de filtros y de diferenciación de montos según el cliente

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,9 @@ Route::middleware('auth')->group(function () {
 	Route::get('api/provinces', [WebController::class, 'apiProvinces'])->name('api.provinces');
 	Route::get('api/districts', [WebController::class, 'apiDistricts'])->name('api.districts');
 
+	Route::get('clients/images', [ClientController::class, 'images'])->name('clients.images');
+	Route::post('clients/images', [ClientController::class, 'uploadImage'])->name('clients.uploadImage');
+	Route::delete('clients/images/{id}', [ClientController::class, 'deleteImage'])->name('clients.deleteImage');
 	Route::get('clients/quotas', [ClientController::class, 'quotas'])->name('clients.quotas');
 	Route::get('clients/contracts', [ClientController::class, 'contracts'])->name('clients.contracts');
 	Route::get('clients/details', [ClientController::class, 'details'])->name('clients.details');
